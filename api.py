69a18967977309dc4d98b632820db3c7ab50b233
krushil <krushil.dhankecha@nexgenarchitects.com>
2024-11-15 06:31:40 +0000

    remove try-catch

--- api.py
+++ api.py
@@ -13,57 +13,57 @@ DATA_PATH = "data"
 
 @app.post("/upload-transcript", tags=["Main"])
 async def load_file(id: str = Form(...), projectId: str = Form(...), file: UploadFile = File(...), videoType: str = Form(...)):
-    try:
+    # try:
 
-        fullpath, filename = await rename_and_save_file(file)
-        response = await load_database(id, projectId, filename, videoType)
+    fullpath, filename = await rename_and_save_file(file)
+    response = await load_database(id, projectId, filename, videoType)
 
-        return JSONResponse(
-            content="File Upload Successfully",
-            status_code=status.HTTP_201_CREATED
-        )
+    return JSONResponse(
+        content="File Upload Successfully",
+        status_code=status.HTTP_201_CREATED
+    )
 
-    except HTTPException as http_exc:
-        return JSONResponse(
-            content=http_exc.detail,
-            status_code=http_exc.status_code
-        )
+    # except HTTPException as http_exc:
+    #     return JSONResponse(
+    #         content=http_exc.detail,
+    #         status_code=http_exc.status_code
+    #     )
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
 
-        return JSONResponse(
-            content=f"Internal server error: {str(e)}",
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+    #     return JSONResponse(
+    #         content=f"Internal server error: {str(e)}",
+    #         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+    #     )
 
-    finally:
-        delete_document(DATA_PATH, file.filename)
+    # finally:
+    #     delete_document(DATA_PATH, file.filename)
 
 
 
 @app.post("/generate-theme", tags=["Main"])
 async def generate_theme(projectId: str = Form(...), prompt: str = Form(...)):
 
-    try:
+    # try:
     
-        response = await generate_theme_details(projectId, prompt)
+    response = await generate_theme_details(projectId, prompt)
 
-        return JSONResponse(
-            content=response,
-            status_code=status.HTTP_200_OK
-        )
+    return JSONResponse(
+        content=response,
+        status_code=status.HTTP_200_OK
+    )
 
-    except HTTPException as http_exc:
-        return JSONResponse(
-            content=http_exc.detail,
-            status_code=http_exc.status_code
-        )
+    # except HTTPException as http_exc:
+    #     return JSONResponse(
+    #         content=http_exc.detail,
+    #         status_code=http_exc.status_code
+    #     )
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
 
-        return JSONResponse(
-            content=f"Internal server error: {str(e)}",
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+    #     return JSONResponse(
+    #         content=f"Internal server error: {str(e)}",
+    #         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+    #     )
