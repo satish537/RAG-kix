1f6bddf1830239b5897dda70f5003fd82541ce23
krushil <krushil.dhankecha@nexgenarchitects.com>
2024-11-18 09:15:04 +0000

    add exception case for theme and description

--- services/retriever.py
+++ services/retriever.py
@@ -18,7 +18,7 @@ QUESTION: {userPrompt}
 
 Identify a theme from the provided transcript and describe it concisely in 1-3 sentences.
 Additionally, explain the rationale behind the title of the theme, ensuring to include exact phrases or sentences from the transcript that directly influenced your decision.When citing supporting text, please provide word-for-word excerpts from the transcript to substantiate your theme identification.
-Return the all in the format specified below.
+Generate supportive text that strictly matches the provided transcript.Ensure that every line corresponds 100% with the transcript without any grammatical mistakes.Do not include ellipses (...) or any additional text within parentheses or braces.The output must consist solely of lines that directly match the transcript, maintaining a perfect alignment with the original content.
 
 Theme: [title of theme]\\Description: [description of theme]\\Supporting texts: - [exact matching text block from the transcript, ensuring it includes a point from the same context/transcript]- [exact matching text block from the transcript, ensuring it includes a point from the same context/transcript]- [exact matching text block from the transcript, ensuring it includes a point from the same context/transcript]
  
@@ -38,10 +38,10 @@ async def generate_theme_details(projectID: str, prompt: str, questionId, partic
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embedding_function())
 
     if questionId and participantId:
-        results = db.similarity_search_with_score(prompt, k=4, filter={"$and": [{"questionId": questionId}, {"participantId": participantId}]})
+        results = db.similarity_search_with_score(prompt, k=3, filter={"$and": [{"questionId": questionId}, {"participantId": participantId}]})
     else:
-        results = db.similarity_search_with_score(prompt, k=4, filter={"projectId": projectID})
-    print(results)
+        results = db.similarity_search_with_score(prompt, k=3, filter={"projectId": projectID})
+
     if not results:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"projectId '{projectID}' does not have any matching data")
 
@@ -52,6 +52,9 @@ async def generate_theme_details(projectID: str, prompt: str, questionId, partic
     response_text = ollamaModel.invoke(final_prompt)
     language_code, language_name = language_detaction(response_text)
     response_obj = await extract_single_input(response_text)
+
+    if not response_obj["theme"] or not response_obj["description"]:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"theme and description are not extracted from llm response")
 
     response_obj = {
         'data': response_obj,
